4ed714f752232391b49fb815259f3e3eed06c9d4
none <none@localhost>
2014-11-29 01:52:17 -0500

    Created an IgnoreCollision script, MirrorMove script, and a PlaySoundOnCollision script. Updated the PlayerMove script as well
    The player move script was updated to settle on rounded numbers after you finish
    moving.
    
    The ignore collision script will let an object ignore another objects
    collision but you need to to add it to both if you want them both to ignore
    collision.
    
    The MirrorMove script will mirror the movement of another game object. This also
    settles the object on rounded numbers.
    
    The PlaySoundOnCollision script will play the sound on collision. It needs an
    audio source on the object though too.

---
+++ Assets/MirrorMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MirrorMove : MonoBehaviour {
+	private Vector3 mirrorPosition;
+	public GameObject ObjectToMirror;
+	// Use this for initialization
+	void Start () {
+		mirrorPosition = ObjectToMirror.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var delta = ObjectToMirror.transform.position - mirrorPosition;
+		mirrorPosition = ObjectToMirror.transform.position;
+
+		this.transform.position -= delta;
+
+        if (delta == Vector3.zero)
+        {
+            this.transform.position = new Vector3(Mathf.Round(this.transform.position.x),
+                                                       Mathf.Round(this.transform.position.y),
+                                                       Mathf.Round(this.transform.position.z));
+        }
+	}
+}
---
+++ Assets/PlaySoundOnCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnCollision : MonoBehaviour {
+
+    public AudioClip Sound;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        audio.PlayOneShot(Sound);
+    }
+}
