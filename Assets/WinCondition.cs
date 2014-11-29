0974cbd326aebf4ebce0e3aa7f479a8490c3142d
ryusage <blaquer@gmail.com>
2014-11-28 19:35:14 -0500

    Added win condition

---
+++ Assets/WinCondition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinCondition : MonoBehaviour {
+	public string NextLevel;
+	public string PlayerObjectName = "Player";
+	public string GoalObjectName = "Lepton";
+
+	private PlayerMove playerObject;
+	private PlayerMove goalObject;
+
+	// Use this for initialization
+	void Start () {
+		playerObject = (PlayerMove)GameObject.Find (PlayerObjectName).GetComponent<PlayerMove>();
+		goalObject = (PlayerMove)GameObject.Find (GoalObjectName).GetComponent<PlayerMove>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerObject.transform.position == goalObject.transform.position && !playerObject.IsMoving && !goalObject.IsMoving) {
+			TriggerWin();
+		}
+	}
+
+	private void TriggerWin() {
+		Application.LoadLevel (NextLevel);
+	}
+}
