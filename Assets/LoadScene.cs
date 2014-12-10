e7b4300944d60e2184d598cc9864b62f00fe787b
Charlie <charlesguse@gmail.com>
2014-12-09 22:26:43 -0500

    Working on getting the next scene to load when a select event is fired. It isn't working yet.

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+	GameObject Scene;
+	// Use this for initialization
+	void Select(string name) {
+		Application.LoadLevel(name);
+	}
+}
