fe015d564580051b05d6591d7250bf5796c09ea8
Charlie Guse <charlesguse@users.noreply.github.com>
2014-12-09 23:46:15 -0500

    Have the start level working part of the way.

---
+++ Assets/LoadSceneEvent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LoadSceneEvent : MonoBehaviour {
+
+	public void LoadScene (string name)
+	{
+		Debug.Log ("UP IN HURRR");
+		//Application.LoadLevel (name);
+	}
+}
