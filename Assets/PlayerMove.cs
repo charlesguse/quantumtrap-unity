4dfe4d6e7efe2fc06277fc6d7880e34f6619e9ac
Charlie Guse <charlesguse@users.noreply.github.com>
2014-12-28 18:09:51 -0500

    Working on fixing the player/lepton movement. Had graphics ordering issues with the scene that are now fixed as well.
    The movement is currently broken.

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -6,8 +6,7 @@ namespace Player
     public class PlayerMove : MonoBehaviour
     {
 
-        public float Velocity = 1.0f;
-        public bool InvertMovement = false;
+        public float Velocity = 2.0f;
         public AudioClip PlayerMovingSound;
 
         public bool IsMoving
@@ -18,30 +17,29 @@ namespace Player
             }
         }
 
-        private int _positionChange = 1;
         private Vector3? _targetPosition;
         private Vector3 _startPosition;
         
         // ReSharper disable once UnusedMember.Local
         void Start()
         {
-            _positionChange = (InvertMovement) ? -1 : 1;
+            _startPosition = transform.position;
         }
 
         // ReSharper disable once UnusedMember.Local
         void Update()
         {
-            if (_targetPosition == null)
+            if (!_targetPosition.HasValue)
             {
                 if (Input.GetAxis("Horizontal") > 0)
-                    _targetPosition = new Vector3(transform.position.x + _positionChange, transform.position.y, 0);
+                    _targetPosition = new Vector3(transform.position.x + 1, transform.position.y, 0);
                 else if (Input.GetAxis("Horizontal") < 0)
-                    _targetPosition = new Vector3(transform.position.x - _positionChange, transform.position.y, 0);
+                    _targetPosition = new Vector3(transform.position.x - 1, transform.position.y, 0);
 
                 if (Input.GetAxis("Vertical") > 0)
-                    _targetPosition = new Vector3(transform.position.x, transform.position.y + _positionChange, 0);
+                    _targetPosition = new Vector3(transform.position.x, transform.position.y + 1, 0);
                 else if (Input.GetAxis("Vertical") < 0)
-                    _targetPosition = new Vector3(transform.position.x, transform.position.y - _positionChange, 0);
+                    _targetPosition = new Vector3(transform.position.x, transform.position.y - 1, 0);
 
                 if (_targetPosition.HasValue)
                 {
@@ -50,15 +48,12 @@ namespace Player
                 }
             }
 
-            if (_targetPosition != null)
+            if (_targetPosition.HasValue)
             {
                 transform.position = Vector3.MoveTowards(transform.position, _targetPosition.Value, Velocity * Time.deltaTime);
                 if (transform.position == _targetPosition.Value)
                 {
                     _targetPosition = null;
-                    transform.position = new Vector3(Mathf.Round(transform.position.x),
-                        Mathf.Round(transform.position.y),
-                        Mathf.Round(transform.position.z));
                 }
             }
         }
