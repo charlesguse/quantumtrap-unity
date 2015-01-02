1e85d4ae5dd1ca8e1e8490bedc745fd874ae605f
Charlie Guse <charlesguse@users.noreply.github.com>
2015-01-01 23:40:49 -0500

    Working on getting the blue blocks collision working properly.

---
+++ Assets/BlueBlock.cs
@@ -0,0 +1,43 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Assets
+{
+    public class BlueBlock : MonoBehaviour
+    {
+        private static Movement _movement;
+        private static ChangeColor _player;
+        private SpriteRenderer _renderer;
+        private Color _blockColor;
+
+        // ReSharper disable once UnusedMember.Local
+        void Start()
+        {
+            if (_movement == null)
+                _movement = GameObject.Find("GameSceneScripts").GetComponent<Movement>();
+            if (_player == null)
+                _player = GameObject.Find("Player").GetComponent<ChangeColor>();
+            _renderer = GetComponent<SpriteRenderer>();
+            _blockColor = _renderer.color;
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private void Update()
+        {
+            if (_player.CurrentColor == CharacterColor.Blue)
+                _renderer.color = new Color(_blockColor.r, _blockColor.g, _blockColor.b, .3f);
+            else
+                _renderer.color = new Color(_blockColor.r, _blockColor.g, _blockColor.b, 1f);
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            //var color = col.gameObject.GetComponent<ChangeColor>();
+            if (col.gameObject.name == "Player" && _player.CurrentColor != CharacterColor.Blue)
+                _movement.OnPlayerCollisionEnter2D(col);
+            else if (col.gameObject.name == "Lepton" && _player.CurrentColor != CharacterColor.Blue)
+                _movement.OnLeptonCollisionEnter2D(col);
+        }
+    }
+}
