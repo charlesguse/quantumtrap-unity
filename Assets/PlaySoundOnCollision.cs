065b648d8b1580a25b26032430ed6b852f5759d8
Charlie Guse <charlesguse@users.noreply.github.com>
2015-01-04 20:01:46 -0500

    Fixed a bug where static variables were not properly being cleaned up between scenes on the BlockTransistion script. Also added an invisible block for areas where you could potentially go off the side.

--- Assets/PlaySoundOnCollision.cs
+++ Assets/PlaySoundOnCollision.cs
@@ -5,16 +5,11 @@ namespace Audio
 {
     public class PlaySoundOnCollision : MonoBehaviour
     {
-
-        //public AudioClip Sound;
-
         // ReSharper disable once UnusedMember.Local
         void OnCollisionEnter2D(Collision2D col)
         {
             if (col.gameObject.name == "Player" && !audio.isPlaying)
                 audio.Play();
-
-            //audio.PlayOneShot(Sound);
         }
     }
 }
