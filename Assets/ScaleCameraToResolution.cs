607d2e67805220cce5ad2887d37544a2cc4bd20d
Charlie Guse <charlesguse@users.noreply.github.com>
2015-01-04 22:52:57 -0500

    Fixed the main camera. Turned it into a prefab and added a scale camera to resolution script. It is hardcoded to resolution 16:9 which is fine for this game but I would like to see if there is a good way to change it based on what the resolution is set to in the "Game" tab.

---
+++ Assets/ScaleCameraToResolution.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace Camera
+{
+    public class ScaleCameraToResolution : MonoBehaviour
+    {
+
+        // ReSharper disable once UnusedMember.Local
+        private void Start()
+        {
+            // set the desired aspect ratio (the values in this example are
+            // hard-coded for 16:9, but you could make them into public
+            // variables instead so you can set them at design time)
+            const float targetaspect = 16.0f / 9.0f;
+
+            // determine the game window's current aspect ratio
+            float windowaspect = Screen.width / (float)Screen.height;
+
+            // current viewport height should be scaled by this amount
+            float scaleheight = windowaspect / targetaspect;
+
+            // obtain camera component so we can modify its viewport
+            var cameraComponent = GetComponent<UnityEngine.Camera>();
+
+            // if scaled height is less than current height, add letterbox
+            if (scaleheight < 1.0f)
+            {
+                Rect rect = cameraComponent.rect;
+
+                rect.width = 1.0f;
+                rect.height = scaleheight;
+                rect.x = 0;
+                rect.y = (1.0f - scaleheight) / 2.0f;
+
+                cameraComponent.rect = rect;
+            }
+            else // add pillarbox
+            {
+                float scalewidth = 1.0f / scaleheight;
+
+                Rect rect = cameraComponent.rect;
+
+                rect.width = scalewidth;
+                rect.height = 1.0f;
+                rect.x = (1.0f - scalewidth) / 2.0f;
+                rect.y = 0;
+
+                cameraComponent.rect = rect;
+            }
+        }
+    }
+}
